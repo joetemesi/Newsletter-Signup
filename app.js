edf54d7d7148be3b884528694d0c058fb96cfad8
Joseph M. Temesi <joemunyendo@gmail.com>
2023-10-20 12:56:59 +0300

    Handled submission of POST requests to Home Route

--- app.js
+++ app.js
@@ -5,7 +5,27 @@ const request = require("request");
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
 
 app.listen(port, function(){
     console.log("Server started on Port: " + port);
+})
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname+ "/signup.html");
+})
+
+app.post("/", function(req, res){
+    var firstName1 = req.body.firstName;
+    var lastName1 = req.body.lastName;
+    var email1 = req.body.email;
+
+    console.log(firstName1);
+    console.log(lastName1);
+    console.log(email1);
+
+
+
 })
