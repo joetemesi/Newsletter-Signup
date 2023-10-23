45ab01b00fb853a85b5fb06297181abef44dac01
Joseph M. Temesi <joemunyendo@gmail.com>
2023-10-23 13:10:19 +0300

    Added response handling feature to track invalid email addresses

--- app.js
+++ app.js
@@ -7,75 +7,69 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
-app.listen(port, function(){
+app.listen(port, function () {
     console.log("Server started on Port: " + port);
-})
+});
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname+ "/signup.html");
-})
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
 
-app.post("/", function(req, res){
+app.post("/", function (req, res) {
     var firstName1 = req.body.firstName;
     var lastName1 = req.body.lastName;
     var email1 = req.body.email;
 
-    // console.log(firstName1);
-    // console.log(lastName1);
-    // console.log(email1);
-
     var data = {
-       members: [
-        {
-            email_address: email1,
-            status:"subscribed",
-            merge_fields: {
-                FNAME: firstName1,
-                LNAME: lastName1
-            }
-
-        }
-       ]
+        members: [
+            {
+                email_address: email1,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName1,
+                    LNAME: lastName1,
+                },
+            },
+        ],
     };
 
-   const jsonData = JSON.stringify(data);
-   const url = process.env.MAILCHIMP_URL;
-   const options = {
-    method: "POST",
-    auth: process.env.MAILCHIMP_AUTH
-   }
+    const jsonData = JSON.stringify(data);
+    const url = process.env.MAILCHIMP_URL;
+    const options = {
+        method: "POST",
+        auth: process.env.MAILCHIMP_AUTH 
+    };
 
-   const request = https.request(url, options, function(response) {
-    let data = '';
+    const request = https.request(url, options, function (response) {
+        let responseData = ''; // Rename the variable to avoid conflict
 
-    if (response.statusCode == 200){
-        res.sendFile(__dirname+"/success.html");
-    } else {
-        res.sendFile(__dirname+"/failure.html");
-    }
+        response.on("data", function (chunk) {
+            responseData += chunk;
+        });
 
-    response.on("data", function(chunk) {
-        data += chunk;
+        response.on("end", function () {
+            try {
+                const jsonDataResponse = JSON.parse(responseData); // Rename the variable
+                console.log(jsonDataResponse);
+
+                if (response.statusCode === 200 && jsonDataResponse.errors.length === 0) {
+                    res.sendFile(__dirname + "/success.html");
+                } else {
+                    res.sendFile(__dirname + "/failure.html");
+                }
+            } catch (error) {
+                console.error("Error parsing JSON data:", error);
+                res.sendFile(__dirname + "/failure.html");
+            }
+        });
     });
 
-    response.on("end", function() {
-        try {
-            const jsonData = JSON.parse(data);
-            console.log(jsonData);
-        } catch (error) {
-            console.error("Error parsing JSON data:", error);
-        }
+    request.on("error", function (error) {
+        console.error("Error in API request:", error);
     });
+
+    request.write(jsonData);
+    request.end();
 });
-
-request.on("error", function(error) {
-    console.error("Error in API request:", error);
-});
-
-request.write(jsonData);
-request.end();
-
-})
